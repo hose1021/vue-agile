ae71285fb4662a205a39faf9b84cb93e34fd4e02
Michael Hose <hose1021@gmail.com>
2022-06-03 18:01:40 +0300

    wip

--- src/Agile.vue
+++ src/Agile.vue
@@ -215,9 +215,9 @@
 			this.$refs.track.addEventListener('touchstart', this.handleMouseDown)
 			this.$refs.track.addEventListener('touchend', this.handleMouseUp)
 			this.$refs.track.addEventListener('touchmove', this.handleMouseMove)
-			this.$refs.track.addEventListener('mousedown', this.handleMouseDown)
-			this.$refs.track.addEventListener('mouseup', this.handleMouseUp)
-			this.$refs.track.addEventListener('mousemove', this.handleMouseMove)
+			// this.$refs.track.addEventListener('mousedown', this.handleMouseDown)
+			// this.$refs.track.addEventListener('mouseup', this.handleMouseUp)
+			// this.$refs.track.addEventListener('mousemove', this.handleMouseMove)
 
 			// Init
 			this.isSSR = false
@@ -230,9 +230,9 @@
 			this.$refs.track.removeEventListener('touchstart', this.handleMouseDown)
 			this.$refs.track.removeEventListener('touchend', this.handleMouseUp)
 			this.$refs.track.removeEventListener('touchmove', this.handleMouseMove)
-			this.$refs.track.removeEventListener('mousedown', this.handleMouseDown)
-			this.$refs.track.removeEventListener('mouseup', this.handleMouseUp)
-			this.$refs.track.removeEventListener('mousemove', this.handleMouseMove)
+			// this.$refs.track.removeEventListener('mousedown', this.handleMouseDown)
+			// this.$refs.track.removeEventListener('mouseup', this.handleMouseUp)
+			// this.$refs.track.removeEventListener('mousemove', this.handleMouseMove)
 
 			this.disableAutoPlay()
 		},
